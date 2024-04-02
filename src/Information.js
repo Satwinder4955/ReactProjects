fdfc8033396f237a052c8d57f11b8a98eebdb9f3
Satwinder Singh <satwinder4955@gmail.com>
2024-04-02 15:22:42 -0400

    put info
    You can put your information in the input and also when you click specific button you curser focus on that input box.

--- src/Information.js
+++ src/Information.js
@@ -5,8 +5,8 @@ function Information(){
     const ageRef = useRef(null);
     const genderRef = useRef(null);
     const stateRef = useRef(null);
-    const countryRef = stateRef(null);
-    const [activeBtn , setActiveBtn] = '';
+    const countryRef = useRef(null);
+    const [activeBtn , setActiveBtn] = useState('');
 
     const nameHandle = () =>{
         nameRef.current.focus();
@@ -28,5 +28,63 @@ function Information(){
         countryRef.current.focus();
         setActiveBtn("country");
     }
+
+    useEffect(() => {
+        nameRef.current.focus()
+    }, []);
+    
+    return (
+        <div>
+            <h1>Information</h1>
+      <div className="form-control">
+        <input ref={nameRef} type="text" placeholder="Name" />
+      </div>
+      <div className="form-control">
+        <input ref={ageRef} type="text" placeholder="Age" />
+      </div>
+      <div className="form-control">
+        <input ref={genderRef} type="text" placeholder="Gender" />
+      </div>
+      <div className="form-control">
+        <input ref={stateRef} type="text" placeholder="State" />
+      </div>
+      <div className="form-control">
+        <input ref={countryRef} type="text" placeholder="Country" />
+      </div>
+      <div className="btn-container">
+        <button
+          className={activeBtn === "name" ? "active" : null}
+          onClick={nameHandle}
+        >
+          Name
+        </button>
+        <button
+          className={activeBtn === "age" ? "active" : null}
+          onClick={ageHandle}
+        >
+          Age
+        </button>
+        <button
+          className={activeBtn === "gender" ? "active" : null}
+          onClick={genderHandle}
+        >
+          Gender
+        </button>
+        <button
+          className={activeBtn === "state" ? "active" : null}
+          onClick={stateHandle}
+        >
+          State
+        </button>
+        <button
+          className={activeBtn === "country" ? "active" : null}
+          onClick={countryHandle}
+        >
+          Country
+        </button>
+      </div>
+        </div>
+        )
 }
 
+export default Information;
